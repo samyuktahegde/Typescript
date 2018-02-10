6404b50d1fa8b21dac8381e4a4ce0a87eb24bb34
samyuktahegde <hegdesamyukta711@gmail.com>
2018-02-10 13:22:16 +0530

    TypeScript Tryouts

---
+++ app.js
@@ -0,0 +1,8 @@
+var todo = "Pick up dry cleaning";
+var name = "Jess";
+// var container = document.getElementById('container');
+for (var x = 0; x <= 5; x++) {
+    var counter = x;
+}
+console.log(counter);
+var displayName = "Todo #" + todo.id;
---
+++ app.ts
@@ -0,0 +1,10 @@
+let todo: string = "Pick up dry cleaning";
+let name: string = "Jess";
+
+// var container = document.getElementById('container');
+for (var x = 0; x <= 5; x++) {
+    const counter = x;
+}
+console.log(counter);
+
+var displayName = `Todo #${todo.id}`
